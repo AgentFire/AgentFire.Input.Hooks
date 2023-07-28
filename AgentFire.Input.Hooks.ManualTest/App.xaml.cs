6ff1548c35851ba4855f160691dc23d163a1e262
AgentFire <agentfire@mail.ru>
2023-07-28 14:08:34 +0300

    Added a WPF manual test project

---
+++ AgentFire.Input.Hooks.ManualTest/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+namespace ManualTest;
+
+public partial class App : Application { }
---
+++ AgentFire.Input.Hooks.ManualTest/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using AgentFire.Input.Hooks.Events;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace AgentFire.Input.Hooks.ManualTest;
+
+public partial class MainWindow : Window
+{
+    private readonly IDisposable _keyboardHook;
+    private readonly IDisposable _mouseHook;
+
+    public ObservableCollection<Key> Keyboard { get; } = new();
+    public ObservableCollection<MouseButton> Mouse { get; } = new();
+
+    private void OnKey(RawKeyboardEvent ev)
+    {
+        if (ev.WasPressed)
+        {
+            if (!Keyboard.Contains(ev.Key))
+            {
+                Keyboard.Add(ev.Key);
+            }
+        }
+        else
+        {
+            Keyboard.Remove(ev.Key);
+        }
+    }
+    private void OnMouse(RawMouseEvent ev)
+    {
+        if (ev.WasPressed)
+        {
+            if (!Mouse.Contains(ev.MouseButton))
+            {
+                Mouse.Add(ev.MouseButton);
+            }
+        }
+        else
+        {
+            Mouse.Remove(ev.MouseButton);
+        }
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _keyboardHook = Hardware.HookKeyboard(ev => Dispatcher.InvokeAsync(() => OnKey(ev)));
+        _mouseHook = Hardware.HookMouse(ev => Dispatcher.InvokeAsync(() => OnMouse(ev)));
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _keyboardHook.Dispose();
+        _mouseHook.Dispose();
+
+        base.OnClosed(e);
+    }
+}
